01ee8fffaba3918c9e0d7e48a7c182b962b09331
David Halperin <david.halperin@gmail.com>
2025-11-16 21:55:00 +0200

    Update base path for yana repository

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/aurora-event-design/' : '/',
+  base: mode === 'production' ? '/yana/' : '/',
   server: {
     host: "::",
     port: 8080,
